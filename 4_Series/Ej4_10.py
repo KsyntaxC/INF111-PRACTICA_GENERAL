dc2e5645f317df6d1d1f26487470432b2fa09bd7
C_A_R_L_O_S <vargasarcayacarlos@gmail.com>
2024-10-03 02:18:54 -0400

    Error arreglado

--- 4_Series/Ej4_10.py
+++ 4_Series/Ej4_10.py
@@ -1,16 +1,18 @@
-#Serie: 2 7 17 34 62 103 161 238 338 467 ...
-N=int(input("Intro N: "))
-i=0; p=3; a=2; t=0; b=0; j=1; c=0
+# Serie: 2 7 17 34 62 103 161 238 338 467 ...
+N = int(input("Intro N: "))
+i=0; p=3; a=2; t=0; b=0
 while i<N:
     t=t+a
-    print(t,end=" ")
-    while j<=p:
-        if p%j==0:
-            c=c+1
-        c=0
-        j=j+1
-    if c==2:
-        b=p
-    p=p+1
+    print(t, end=" ")
+    e=0
+    while e==0:
+        c=0; j=1
+        while j<=p:
+            if p%j==0:
+                c=c+1
+            j=j+1
+        if c==2:
+            b=p; e=1
+        p=p+1
     a=a+b
     i=i+1
