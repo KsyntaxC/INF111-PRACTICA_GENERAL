4b369a15cd47867961a529dd1f2ddc5dbd31f7b4
C_A_R_L_O_S <vargasarcayacarlos@gmail.com>
2024-10-03 16:22:48 -0400

    errores corregidos

--- 1_Secuenciales/Ej1_10.py
+++ 1_Secuenciales/Ej1_10.py
@@ -3,16 +3,11 @@
 #Donde horas < 24, minutos < 60 segundos < 60. 4
 #Salida: Imprima en la salida la nueva hora del reloj en formato mostrado en el ejemplo.
 #Las horas, minutos y segundos deben estar en el siguiente formato:
-XX = int(input("Ingrese la hora (0-23): "))  
-YY = int(input("Ingrese los minutos (0-59): "))
-ZZ = int(input("Ingrese los segundos (0-59): "))
-ZZ=ZZ+1
-if ZZ==60:
-    ZZ=0
-    YY=YY+1
-if YY==60:
-    YY=0
-    XX=XX+1
-if XX==24:
-    XX=0
-print(f"{XX:02d}:{YY:02d}:{ZZ:02d}")
+XX = int(input("Ingrese la hora: "))  
+YY = int(input("Ingrese los minutos: "))
+ZZ = int(input("Ingrese los segundos: "))
+TS=XX*3600+YY*60+ZZ+1
+h=TS//3600%24
+m=TS//60%60
+s=TS%60
+print(f"{h:02d}:{m:02d}:{s:02d}")
--- 7_Descomposicion/Ej7_8.py
+++ 7_Descomposicion/Ej7_8.py
@@ -1,4 +1,4 @@
-#Dado un numero N imprimir alg´un otro que tenga n digitos, sea multiplo de 3 y sea minimo.
+#Dado un numero N imprimir algun otro que tenga n digitos, sea multiplo de 3 y sea minimo.
 N=int(input("Introduce el número de dígitos n: "))
 n=10**(N-1)
 if n%3!=0:
